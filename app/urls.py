3991284e9b0d2c526bbaa333b49eb093db77dd0a
Ashish001 <ashishchouhan57488@gmail.com>
2023-12-27 08:20:56 +0530

    add favicon icon change in emailForm.html

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('EmailSend/', views.sendMail, name='sendemail'),  
+    path('', views.sendMail, name='sendemail'),  
     
 ]
